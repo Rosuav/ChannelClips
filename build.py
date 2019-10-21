b4450c01c73e08b081a611b332228e227426bab0
Chris Angelico <rosuav@gmail.com>
2019-10-21 23:50:25 +1100

    Build the parser (abusing CSV for this)

--- build.py
+++ build.py
@@ -1,9 +1,31 @@
 # Build the clips!
+import csv
+from dataclasses import dataclass # ImportError? Upgrade to Python 3.7 or pip install dataclasses
+
+@dataclass
+class VideoPiece:
+	slug: str # eg PoorSavageLorisHassaanChop
+	start: float # Start position
+	duration: float # 0.0 = till end of clip
+
+@dataclass
+class TextPiece:
+	text: str
+	duration: float
+
+def remove_comments(iter):
+	for line in iter:
+		line = line.split("#")[0].strip()
+		if line: yield line
 
 # 1) Read and parse the file
-
 def parse_template(fn):
-	...
+	pieces = []
+	with open(fn) as f:
+		for line in csv.reader(remove_comments(f), delimiter=' '):
+			if len(line) == 2: pieces.append(TextPiece(line[0], float(line[1])))
+			else: pieces.append(VideoPiece(line[0], float(line[1]), float(line[2])))
+	return pieces
 
 # 2) Download all clips into the local cache
 def precache(slug):
